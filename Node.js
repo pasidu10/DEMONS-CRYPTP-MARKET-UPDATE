4ed2082e1dccc345c809ae055fabb2ecdfbb43b7
pasidu10 <punnyanamda.thero@gmail.com>
2025-07-09 23:15:52 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,37 @@
+---
+
+Code Overview (Node.js):
+1. *BTC Price Fetching:*
+```js
+const axios = require('axios');
+
+async function getBTCPrice() {
+  const res = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
+  return res.data.bitcoin.usd;
+}
+```
+2.
+```js
+const twilio = require('twilio');
+const client = twilio('YOUR_SID', 'caming soon');
+
+async function sendWhatsApp(price) {
+  await client.messages.create({
+    body: `🪙 BTC 4H Update:\nCurrent Price: 
+    
+{price}`,
+    from: 'whatsapp:+94766359869',
+    to: 'whatsapp:+94722977523' // Group broadcast via approved Twilio sender
+  });
+}
+```
+
+3. *Scheduler:*
+```js
+setInterval(async () => {
+  const price = await getBTCPrice();
+  await sendWhatsApp(price);
+}, 4 * 60 * 60 * 1000); // every 4 hours
+```
+
+---
